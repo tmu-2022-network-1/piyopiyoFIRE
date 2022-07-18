356b93d4d3d56c075e7f62ee6fff07b8ae6b80bd
pppiro <84230554+pppiro@users.noreply.github.com>
2022-07-18 22:16:18 +0900

    とりあえず完成

--- docs/script.js
+++ docs/script.js
@@ -10,6 +10,11 @@ var zoomRate = 1000; //画像のズーム割合
 
 var fadein; //フェードイン開始位置
 var p1; //メインコンテンツのz移動開始位置
+var p2;
+var p3;
+var p4;
+var p5;
+var p6;
 
 function getScreenSize()
 {
@@ -23,14 +28,21 @@ function getScreenSize()
 
 function makeContents()
 {
+    fadein = sh * 1.2;
+    p1 = sh * 4;
+    p2 = p1 + zoomRate;
+    p3 = p2 + zoomRate;
+    p4 = p3 + zoomRate;
+    p5 = p4 + zoomRate;
+    p6 = p5 + zoomRate;
+    p7 = p6 + zoomRate / 2;
+
     //スクロール用の要素の高さを設定
     var scroll = document.getElementById("scroll");
     scroll.style.width = `${sw}px`;
-    scroll.style.height = `${sh * 50}px`;
+    scroll.style.height = `${p6}px`;
 
-    fadein = sh * 1.3;
-    p1 = sh * 4;
-    p2 = p1 + zoomRate;
+    //document.body.style.height = `${p7}px`
 
     var screen = [];
     screen = document.getElementsByClassName("screen");
@@ -43,8 +55,6 @@ function makeContents()
 
     //基準となる枠、画面サイズに合わせて常に中央に配置
     var base = document.getElementById("base");
-    //奥スクロール開始位置を指定
-    //base.style.marginTop = "500px";
     base.style.height = `${sh}px`;
     base.style.width = `${sw}px`;
     base.style.position = "sticky";
@@ -64,11 +74,13 @@ function makeContents()
     }
 
     danchis = document.getElementsByClassName("danchis");
+    danchis2 = document.getElementsByClassName("danchis2")
     if ( sh < sw )
     {
         for ( j=0; j < danchis.length; j++)
         {
             danchis[j].style.width = "100%";
+            danchis2[j].style.width = "100%"
         }
     }
     else
@@ -76,10 +88,11 @@ function makeContents()
         for ( j=0; j < danchis.length; j++)
         {
             danchis[j].style.height = "100%";
+            danchis2[j].style.height = "100%";
         }
     }
 
-    
+    document.getElementById("title2").style.height = "40%";    
 }
 
 function scroll()
@@ -87,12 +100,6 @@ function scroll()
     //スクロール量を取得
     y = window.pageYOffset
     console.log(y);
-    
-    //var scaler = document.getElementById("scaler");
-    //scaler.style.transform = `translateZ(${y / 1000 }px)`
-
-    //スクロール量に応じてコンテンツをz方向に移動
-    //総スクロール量ごとに動かす画像を指定？
 
     var dis = document.getElementById("discription");
 
@@ -127,13 +134,84 @@ function scroll()
 
         con1.style.visibility = "visible"
         con2.style.visibility = "visible"
-        con1.style.transform = `translateZ(${ ( y - p2 ) / zoomRate * 1.3 }px)`
-        con2.style.transform = `translateZ(${ ( y - p2 ) / zoomRate }px)`
+        con1.style.transform = `translateZ(${ ( y - p2 ) / zoomRate }px)`
+        con2.style.transform = `translateZ(${ ( y - p2 ) / zoomRate * 1.3 }px)`
     }
     else
     {
         con1.style.visibility = "hidden"
         con2.style.visibility = "hidden"
+    }
+
+    if ( y > p3 )
+    {
+        let con3 = document.getElementById("con3");
+        let con4 = document.getElementById("con4");
+
+        con3.style.visibility = "visible"
+        con4.style.visibility = "visible"
+        con3.style.transform = `translateZ(${ ( y - p3 ) / zoomRate }px)`
+        con4.style.transform = `translateZ(${ ( y - p3 ) / zoomRate * 1.3 }px)`
+    }
+    else
+    {
+        con3.style.visibility = "hidden"
+        con4.style.visibility = "hidden"
+    }
+
+    if ( y > p4 )
+    {
+        let con5 = document.getElementById("con5");
+        let con6 = document.getElementById("con6");
+
+        con5.style.visibility = "visible"
+        con6.style.visibility = "visible"
+        con5.style.transform = `translateZ(${ ( y - p4 ) / zoomRate }px)`
+        con6.style.transform = `translateZ(${ ( y - p4 ) / zoomRate * 1.3 }px)`
+        
+    }
+    else
+    {
+        con5.style.visibility = "hidden"
+        con6.style.visibility = "hidden"
+    }
+
+    
+    if ( y > p5 )
+    {
+        let danchis2 = document.getElementsByClassName("danchis2");
+        for ( b=0; b < danchis2.length; b++)
+        {
+            danchis2[b].style.visibility = "visible";
+            danchis2[b].style.transform = `translateZ(${ 0.9 - (y - p5) / zoomRate }px)`;
+
+            if ( (0.9 - (y-p5) / zoomRate) < 0)
+            {
+                danchis2[b].style.transform = "translateZ(0px)";
+            }
+        }
+    }
+    else
+    {
+        for (b=0; b < danchis2.length; b++)
+        {
+            danchis2[b].style.visibility = "hidden"
+        }
+    }
+
+    if ( y > p6 )
+    {
+        let title2 = document.getElementById("title2");
+        title2.style.visibility = "visible";
+        title2.classList.add("fadeIn");
+        document.getElementById("base").style.position = "fixed";
+    }
+    else
+    //上に戻ったら再びフェードインするように設定
+    {
+        title2.style.visibility = "hidden"
+        title2.classList.remove("fadeIn");
+        document.getElementById("base").style.position = "sticky";
     }
 
     //コンテンツのz座標を取得
